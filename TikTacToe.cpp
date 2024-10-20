06c5938f8b97d77ac4c229570501103f4a3ce70b
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2024-10-20 16:08:55 -0700

    Update TikTacToe.cpp

--- TikTacToe.cpp
+++ TikTacToe.cpp
@@ -2,64 +2,62 @@
 
 using namespace std;
 
-char board[3][3] = {
-    {' ', ' ', ' '},
-    {' ', ' ', ' '},
-    {' ', ' ', ' '}
-};
-
-// Function to display the board
-void displayBoard() {
-    cout << "   1 2 3\n";
-    for (int i = 0; i < 3; i++) {
-        cout << i + 1 << "  ";
-        for (int j = 0; j < 3; j++) {
-            cout << board[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-// Function to check if a move is valid
-bool isValidMove(int row, int col) {
-    return board[row - 1][col - 1] == ' ';
-}
-
-// Function to check for a win
-bool checkWin(char player) {
-    // Check rows, columns, and diagonals
-    for (int i = 0; i < 3; i++) {
-        if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
-            (board[0][i] == player && board[1][i] == player && board[2][i] == player) ||
-            (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
-            (board[0][2] == player && board[1][1] == player && board[2][0] == player)) {
-            return true;
-        }
-    }
-    return false;
-}
-
-// Function to check for a tie
-bool checkTie() {
-    for (int i = 0; i < 3; i++) {
-        for (int j = 0; j < 3; j++) {
-            if (board[i][j] == ' ') {
-                return false;
-            }
-        }
-    }
-    return true;
-}
-
-int main() {
+//Tic-Tac-Toe game
+void playTicTacToe() {
+    char board[3][3] = {{' ', ' ', ' '},
+                        {' ', ' ', ' '},
+                        {' ', ' ', ' '}};
     char currentPlayer = 'X';
     bool gameOver = false;
+
+    //displays the board
+    void displayBoard() {
+        cout << "   1 2 3\n";
+        for (int i = 0; i < 3; i++) {
+            cout << i + 1 << "  ";
+            for (int j = 0; j < 3; j++) {
+                cout << board[i][j] << " ";
+            }
+            cout << endl;
+        }
+    }
+
+    // check if a move is valid
+    bool isValidMove(int row, int col) {
+        return board[row - 1][col - 1] == ' ';
+    }
+
+    //Check for a win
+    bool checkWin(char player) {
+        for (int i = 0; i < 3; i++) {
+            if ((board[i][0] == player && board[i][1] == player && board[i][2] == player) ||
+                (board[0][i] == player && board[1][i] == player && board[2][i] == player) ||
+                (board[0][0] == player && board[1][1] == player && board[2][2] == player) ||
+                (board[0][2] == player && board[1][1] == player && board[2][0] == player))  
+ {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //check for a tie
+    bool checkTie() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (board[i][j] == ' ') {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
 
     while (!gameOver) {
         displayBoard();
 
         int row, col;
-        cout << "Player " << currentPlayer << ", enter your move (row col): ";
+        cout << "Player " << currentPlayer << ", enter your move : ";
         cin >> row >> col;
 
         if (isValidMove(row, col)) {
@@ -77,5 +75,25 @@ int main() {
         }
     }
 
+    // asks the player if they want to play TikTacToe again
+    char playAgain;
+    cout << "Do you want to play again? Y/N: ";
+    cin >> playAgain;
+    if (playAgain  
+ == 'y' || playAgain == 'Y') {
+        // Reset the board and start a new game
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                board[i][j]  
+ = ' ';
+            }
+        }
+        currentPlayer = 'X';
+        gameOver = false;
+    }
+}
+
+int main() {
+    playTicTacToe();
     return 0;
 }
