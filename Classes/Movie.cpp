0e27133fe6fbfee1429c5aa830268e4f6ff0f107
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2024-11-19 18:51:35 -0800

    Update Movie.cpp

--- Classes/Movie.cpp
+++ Classes/Movie.cpp
@@ -2,14 +2,18 @@
 #include <iostream>
 using namespace std;
 
-Movie::Movie(string title, int year, string publisher,
-             string director, int duration, string rating)
-    : Media(title, year, publisher), director(director), 
-      duration(duration), rating(rating) {}
+Movie::Movie(const char* title, int year, const char* publisher,
+             const char* director, int duration, const char* rating)
+    : Media(title, year, publisher), duration(duration) {
+    strncpy(this->director, director, MAX_STRING - 1);
+    this->director[MAX_STRING - 1] = '\0';
+    strncpy(this->rating, rating, MAX_STRING - 1);
+    this->rating[MAX_STRING - 1] = '\0';
+}
 
-string Movie::getDirector() const { return director; }
+const char* Movie::getDirector() const { return director; }
 int Movie::getDuration() const { return duration; }
-string Movie::getRating() const { return rating; }
+const char* Movie::getRating() const { return rating; }
 
 void Movie::displayInfo() const {
     cout << "Movie: " << title << "\n"
