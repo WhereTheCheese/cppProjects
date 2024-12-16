a84ff8137dc934195792be9316b9962498004c77
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2024-12-15 18:00:31 -0800

    Create Student.h

---
+++ LinkedLists/Student.h
@@ -0,0 +1,17 @@
+#ifndef STUDENT_H
+#define STUDENT_H
+
+using namespace std;
+
+class Student {
+private:
+//student id
+    int id; 
+
+public:
+//constructor for Student
+    Student(int id) : id(id) {} 
+    
+};
+
+#endif // STUDENT_H
