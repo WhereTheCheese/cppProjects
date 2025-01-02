26ce17fedbe2f3cb8e95f7432315ffe3d2b42133
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-01 21:47:27 -0800

    Update Movie.h

--- Classes/Movie.h
+++ Classes/Movie.h
@@ -2,23 +2,18 @@
 #define MOVIE_H
 #include "Media.h"
 
-class Movie : public Media {
+class Movie : public Media {  
 private:
-     char director[MAX_STRING];
+    char director[MAX_STRING];
     int duration;
     char rating[MAX_STRING];
 
 public:
-    //Constructor
     Movie(const char* title = "", int year = 0, const char* publisher = "",
           const char* director = "", int duration = 0, const char* rating = "");
-    
-    //getters
     const char* getDirector() const;
     int getDuration() const;
     const char* getRating() const;
-    
-    //Override display method
     void displayInfo() const override;
 };
 #endif
