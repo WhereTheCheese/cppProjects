e834d31fca6706d7e9868bd0751cf7da2d0f515a
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-01 21:45:04 -0800

    Update Media.h

--- Classes/Media.h
+++ Classes/Media.h
@@ -3,7 +3,7 @@
 #include <cstring>
 using namespace std;
 
-const int MAX_STRING = 100;  
+const int MAX_STRING = 100;  // Define this in Media.h so all files can access it
 
 class Media {
 protected:
@@ -12,17 +12,12 @@ protected:
     char publisher[MAX_STRING];
 
 public:
-    //constructor
-   Media(const char* title = "", int year = 0, const char* publisher = "");
-    
-    //Virtual destructor
+    Media(const char* title = "", int year = 0, const char* publisher = "");
     virtual ~Media() = default;
     
-    // Virtual methods for common fields
     virtual const char* getTitle() const;
     virtual int getYear() const;
-    virtual string getPublisher() const;
-    
-    // Pure virtual method to make this an abstract class
+    virtual const char* getPublisher() const;
     virtual void displayInfo() const = 0;
-}; #endif
+};
+#endif
