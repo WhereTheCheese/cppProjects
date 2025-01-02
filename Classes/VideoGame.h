e00de2a67be11ec79960938dba15c091813bd75f
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-01 21:41:39 -0800

    Update VideoGame.h

--- Classes/VideoGame.h
+++ Classes/VideoGame.h
@@ -1,15 +1,18 @@
-#ifndef VIDEOGAME_H
-#define VIDEOGAME_H
-#include "Media.h"
+#include "VideoGame.h"
+#include <iostream>
+using namespace std;
 
-class VideoGame : public Media {
-private:
-    char rating[MAX_STRING];
+VideoGame::VideoGame(const char* title, int year, const char* publisher, const char* rating)
+    : Media(title, year, publisher) {
+    strncpy(this->rating, rating, MAX_STRING - 1);
+    this->rating[MAX_STRING - 1] = '\0';
+}
 
-public:
-    VideoGame(const char* title = "", int year = 0, const char* publisher = "", 
-             const char* rating = "");
-    const char* getRating() const;
-    void displayInfo() const override;
-};
-#endif
+const char* VideoGame::getRating() const { return rating; }
+
+void VideoGame::displayInfo() const {
+    cout << "Video Game: " << title << "\n"
+         << "Year: " << year << "\n"
+         << "Publisher: " << publisher << "\n"
+         << "Rating: " << rating << "\n";
+}
