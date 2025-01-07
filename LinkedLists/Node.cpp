77f4dcffa8cea75e32a47990c4c3188a0bfb2c6e
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-06 18:10:22 -0800

    Update Node.cpp

--- LinkedLists/Node.cpp
+++ LinkedLists/Node.cpp
@@ -1,23 +1,23 @@
 #include "Node.h"
 
-//constructor, This sets up a new node with a studnt
 Node::Node(Student* s) {
     student = s;
-    next = nullptr; //start of list or end if last node
+    next = nullptr;
 }
 
+Node::~Node() {
+    delete student;  // Clean up the student data
 
-//Returns next node in list
+}
+
 Node* Node::getNext() {
     return next;
 }
 
-//Returns the Student that this Node holds
 Student* Node::getStudent() {
     return student;
 }
 
-//changes locatioin Node points next
 void Node::setNext(Node* newNext) {
     next = newNext;
 }
