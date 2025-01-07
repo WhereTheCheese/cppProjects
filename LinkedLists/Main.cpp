a4b5c56e6a56cf11468f56b931f9a1b403466df2
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-06 18:09:44 -0800

    Update Main.cpp

--- LinkedLists/Main.cpp
+++ LinkedLists/Main.cpp
@@ -1,3 +1,4 @@
+
 #include <iostream>
 #include "Node.h"
 #include "Student.h"
@@ -5,27 +6,26 @@
 using namespace std;
 
 int main() {
-    //Make the students
+    // Create student objects + Nodes for those students.
     Student* student1 = new Student(101);
     Student* student2 = new Student(102);
 
-    //make nodes with those students
     Node* node1 = new Node(student1);
     Node* node2 = new Node(student2);
 
-    //Link the nodes together
+    //Link the nodes, can add more here for part 2 and make a linked list (insert mind blown emoji)
     node1->setNext(node2);
 
-    //tests
-    cout << "First student's ID: " << node1->getStudent()->id << endl;
-    cout << "Second student's ID: " << node2->getStudent()->id << endl;
-    
-    Node* nextNode = node1->getNext();
-    cout << "ID of the student in the next Node: " << nextNode->getStudent()->id << endl;
+    //Test the functions
+    cout << "First student's ID: " << node1->getStudent()->getId() << endl;
+    cout << "Second student's ID: " << node2->getStudent()->getId() << endl;
 
-    //clean up
-    delete node1;
+    Node* nextNode = node1->getNext();
+    cout << "ID of the student in the next Node: " << nextNode->getStudent()->getId() << endl;
+
+    // Clean up
     delete node2;
+    delete node1;
 
     return 0;
 }
