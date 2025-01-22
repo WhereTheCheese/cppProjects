08069604a71f2e36998c079fe3057d73b5c4a1a0
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-21 22:34:57 -0800

    Update Media.cpp

--- Classes/Media.cpp
+++ Classes/Media.cpp
@@ -1,15 +1,31 @@
 #include "Media.h"
 #include <cstring>
 
-Media::Media(const char* title, int year, const char* publisher) {
-    strncpy(this->title, title, MAX_STRING - 1);
-    this->title[MAX_STRING - 1] = '\0';
-    this->year = year;
-    strncpy(this->publisher, publisher, MAX_STRING - 1);
-    this->publisher[MAX_STRING - 1] = '\0';
+Media::Media() {
+    title[0] = '\0';
+    publisher[0] = '\0';
+    year = 0;
 }
 
-const char* Media::getTitle() const { return title; }
-int Media::getYear() const { return year; }
-const char* Media::getPublisher() const { return publisher; }
+Media::~Media() {}
 
+void Media::setTitle(const char* t) {
+    strncpy(title, t, 99);
+    title[99] = '\0';
+}
+void Media::setYear(int y) {
+    year = y;
+}
+void Media::setPublisher(const char* p) {
+    strncpy(publisher, p, 99);
+    publisher[99] = '\0';
+}
+const char* Media::getTitle() const {
+    return title;
+}
+int Media::getYear() const {
+    return year;
+}
+const char* Media::getPublisher() const {
+    return publisher;
+}
