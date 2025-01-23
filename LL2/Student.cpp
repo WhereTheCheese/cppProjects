c236559e179815363d04c3c48a8e48944d03ae36
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-01-23 09:35:54 -0800

    Update Student.cpp
    I did it

--- LL2/Student.cpp
+++ LL2/Student.cpp
@@ -1,54 +1,21 @@
 #include "Student.h"
-#include <cstring>
+using namespace std;
 
-Student::Student() {
-    //start with emptu strings
-    firstName = new char[1];
-    firstName[0] = '\0';
-    lastName = new char[1];
-    lastName[0] = '\0';
-    id = 0;
-    gpa = 0.0;
-}
+Student::Student(const std::string& firstName, const std::string& lastName, int id, double gpa)
+     firstName(firstName), lastName(lastName), id(id), gpa(gpa) {}
 
-Student::~Student() {
-    //dynamicly allocated memory
-    delete[] firstName;
-    delete[] lastName;
-}
-
-char* Student::getFirstName() {
+string Student::getFirstName() const {
     return firstName;
 }
 
-char* Student::getLastName() {
+string Student::getLastName() const {
     return lastName;
 }
 
-int Student::getID() {
+int Student::getID() const {
     return id;
 }
 
-float Student::getGPA() {
+double Student::getGPA() const {
     return gpa;
-
-void Student::setFirstName(const char* name) {
-    //Delete old first name 
-    delete[] firstName;
-    firstName = new char[strlen(name) + 1];
-    strcpy(firstName, name);
 }
-
-void Student::setLastName(const char* name) {
-    //old last name
-    delete[] lastName;
-    lastName = new char[strlen(name) + 1];
-    strcpy(lastName, name);
-}
-
-void Student::setID(int newID) {
-    id = newID;
-}
-
-void Student::setGPA(float newGPA) {
-    gpa = newGPA;
