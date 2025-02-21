641eafd70abf0f82067e4461b2bfee797d571476
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-02-20 17:37:30 -0800

    Update Heap.cpp

--- heap/Heap.cpp
+++ heap/Heap.cpp
@@ -1,1 +1,25 @@
+#include <iostream>
 
+using namespace std; 
+
+class Heap {
+    private:
+    //creates the initial heap array
+        int heap[101];
+        int size;
+   
+        public:
+     
+    Heap() {
+        size = 0;
+    }
+    //all of the functions that are used to control the program
+    void insert(int value);
+    void deleteMax();
+    void print();
+    void display();
+    void heapify(int i);
+    
+        };
+
+    
