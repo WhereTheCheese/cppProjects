334c5ecf39062bd2378f4be4852778008e479dc7
Andrew <93689827+WhereTheCheese@users.noreply.github.com>
2025-02-20 18:01:49 -0800

    Update Heap.cpp
    I tryed to ask a freind for advice on heap and he just kept saying the best way to work on heap was to stay on top of things (cuz its like a max heap) HAHAHAHAHAHA

--- heap/Heap.cpp
+++ heap/Heap.cpp
@@ -17,9 +17,50 @@ class Heap {
     void insert(int value);
     void deleteMax();
     void print();
-    void display();
     void heapify(int i);
+    void removeAll();
     
         };
 
+//inserts a value into the heap
+void Heap::insert(int value) {
+    size++;
+    heap[size] = value; 
+    int i = size;
+    while (i > 1 && heap[i] > heap[i/2]) {
+        swap(heap[i], heap[i/2]);
+        i = i/2;
+    }
+}
+
+//deletes the maximum value from the heap
+void Heap::deleteMax() {
+    heap[1] = heap[size];
+    size--;
+    heapify(1);
+}
     
+//prints the heap
+void Heap::print(int index, int tab = 0) {
+    if (index > size) {
+        return;
+    }
+    print(2*index + 1, tab + 1);
+    for (int i = 0; i < tab; i++) {
+        cout << " ";
+    }
+    cout << heap[index] << endl;
+    print(2*index, tab + 1);
+}
+
+//removes all values from the heap
+void Heap::removeAll() {
+    size = 0;
+}
+
+//heapifies the heap
+void Heap::heapify(int i) {
+//gonna work on this later tbh
+}
+
+
